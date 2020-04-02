d8358f180d1d1891df7d498c8a8d4296be55e8c6
anhnd3 <anhnd3@vng.com.vn>
2020-04-02 14:21:52 +0700

    Update func add httpcode header

--- http_response.go
+++ http_response.go
@@ -2,12 +2,35 @@ package grpc_gateway_common
 
 import (
 	"context"
-	"github.com/golang/protobuf/proto"
 	"net/http"
+	"strconv"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
+
+const httpCodeHeader = "X-Http-Code"
 
 // FormatHTTPResponse support sync cookie to response
 func FormatHTTPResponse(ctx context.Context, w http.ResponseWriter, resp proto.Message) error {
-	// md, _ := runtime.ServerMetadataFromContext(ctx)
+	md, ok := runtime.ServerMetadataFromContext(ctx)
+	if !ok {
+		return nil
+	}
+	if vals := md.HeaderMD.Get(httpCodeHeader); len(vals) > 0 {
+		code, err := strconv.Atoi(vals[0])
+		if err != nil {
+			return err
+		}
+		w.WriteHeader(code)
+	}
 	return nil
+}
+
+// SetHTTPCodeHeader ...
+func SetHTTPCodeHeader(ctx context.Context, code string) {
+	md := metadata.Pairs(httpCodeHeader, code)
+	grpc.SetHeader(ctx, md)
 }
